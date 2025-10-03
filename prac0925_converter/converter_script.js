4c64e74d75abec0829545b46b9a52357f05d10e2
Min Derrida <min.derrida@gmail.com>
2025-10-03 15:40:29 +0100

    Update converter_script.js

--- prac0925_converter/converter_script.js
+++ prac0925_converter/converter_script.js
@@ -1,39 +1,36 @@
 const form = document.getElementById("currency_converter");
-form.addEventListener("submit", async(event) =>{
-    event.preventDefault();
-    const from = document.getElementById("from_currency").value;
-    const to = document.getElementById("to_currency").value;
-    const from_amount = parseFloat(document.getElementById("from_amount").value);
+form.addEventListener("submit", async function calculate() {
+
+    calculate.preventDefault();
+
+    const from_currency = document.getElementById("from_currency").value;
+    const to_currency = document.getElementById("to_currency").value;
+    const from_amount = parseFloat(document.getElementById("from_amount"));
+    const unit_symbol = document.getElementById("from_unit").value;
     
     if (typeof(from_amount) !== 'number' || isNaN(from_amount)) {
         alert("Please enter an amount.");
         return;
     }
 
-const unit_symbol = document.getElementById("from_unit");
-
-const symbols = {
-  USD: "$",
-  EUR: "€",
-  GBP: "£",
-  CNY: "¥"
-};
-
-from.addEventListener("change", function() {
-    const curreny_code = from.value;
-    unit_symbol.textContent = symbols[curreny_code] || "?";
-  });
+    const symbols = {
+    USD: "$",
+    EUR: "€",
+    GBP: "£",
+    CNY: "¥"
+    };
 
     try {
         const url = `https://api.frankfurter.dev/v1/latest?base=${encodeURIComponent(from)}&symbols=${encodeURIComponent(to)}`;
         
         fetch(url)
-            .then((response) => response.json())
-            .then((data) => {
-                const rate = data.rates[to];
-                const to_amount = rate * from_amount;
-                document.getElementById("result").textContent = to_amount + " " + to;
-            })
+            .then(function response(){
+                 const rate = response.rates[to_currency];
+                 const to_amount = rate * from_amount;
+                document.getElementById("result").textContent = to_amount + " " + to;)
+            }
+             
+               
             .catch((error) => {
                 alert("Error fetching exchange rate.");
                 console.error(error);
