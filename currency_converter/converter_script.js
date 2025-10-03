96d1e39695832a46c2b1cf68135ba3623ff1a4af
Min Derrida <min.derrida@gmail.com>
2025-10-03 22:11:14 +0100

    1

--- currency_converter/converter_script.js
+++ currency_converter/converter_script.js
@@ -1,39 +1,55 @@
 const form = document.getElementById("currency_converter");
-from_currency.addEventListener("change", function() {
-    const symbols = {
-    USD: "$",
-    EUR: "€",
-    GBP: "£",
-    CNY: "¥"
+    const fromCurrency = document.getElementById("from_currency");
+    const toCurrency = document.getElementById("to_currency");
+    const fromUnit = document.getElementById("from_unit");
+    const fromUnitImg = document.getElementById("from_unit_img");
+    const toUnit = document.getElementById("to_unit");
+    const toUnitImg = document.getElementById("to_unit_img");
+
+    fromCurrency.addEventListener("change", function() {
+    const symbolsImg = {
+        USD: '<img src="img/dollar.png" alt="dollar" width="25px"/>',
+        EUR: '<img src="img/euro.png" alt="euro" width="25px"/>',
+        GBP: '<img src="img/pound.png" alt="pound" width="25px"/>',
+        CNY: '<img src="img/yuan.png" alt="yuan" width="25px"/>'
     };
-    document.getElementById("from_unit").textContent = symbols[from_currency.value] || "";})
-
-form.addEventListener("submit", async function calculate(event) {
-
-    event.preventDefault();
-
-    const from_currency = document.getElementById("from_currency").value;
-    const to_currency = document.getElementById("to_currency").value;
-    const from_amount = parseFloat(document.getElementById("from_amount").value);
+    fromUnitImg.innerHTML = symbolsImg[fromCurrency.value] || "";
+    fromUnit.textContent = fromCurrency.value || "";})
     
-    if (typeof(from_amount) !== 'number' || isNaN(from_amount)) {
-        alert("Please enter an amount.");
-        return;
-    }
+    toCurrency.addEventListener("change", function() {
+    const symbolsImg = {
+        USD: '<img src="img/dollar.png" alt="dollar" width="25px"/>',
+        EUR: '<img src="img/euro.png" alt="euro" width="25px"/>',
+        GBP: '<img src="img/pound.png" alt="pound" width="25px"/>',
+        CNY: '<img src="img/yuan.png" alt="yuan" width="25px"/>'
+    };
+    toUnitImg.innerHTML = symbolsImg[toCurrency.value] || "";
+    toUnit.textContent = toCurrency.value || "";})
 
-
+    form.addEventListener("submit", async function calculate(event) {
+    
+    event.preventDefault();
+    const fromAmount = parseFloat(document.getElementById("from_amount").value);
+    
+        if (isNaN(fromAmount) || fromAmount < 0) {
+            alert("Please enter a valid amount.");
+            return;
+        }
+        if (fromCurrency.value === toCurrency.value) {
+           document.getElementById("result").textContent = `${(fromAmount).toFixed(2)}`;
+    }  
+    else{
     try {
-        const url = `https://api.frankfurter.dev/v1/latest?base=${encodeURIComponent(from_currency)}&symbols=${encodeURIComponent(to_currency)}`;
+        const url = `https://api.frankfurter.dev/v1/latest?base=${encodeURIComponent(fromCurrency.value)}&symbols=${encodeURIComponent(toCurrency.value)}`;
         
         const response = await fetch(url);
         const data = await response.json();
-        const rate = data.rates[to_currency];
-        const to_amount = rate * from_amount;
-         document.getElementById("result").textContent = `${to_currency} ${to_amount.toFixed(2)}`;
-             
-
-            } catch (error) {
-                alert("Error fetching exchange rate.");
-                console.error(error);
-            }}
-);
+        const rate = data.rates[toCurrency.value];
+        const toAmount = rate * fromAmount;
+        document.getElementById("result").textContent = `${toCurrency.value} ${toAmount.toFixed(2)}`;
+    } catch (error) {
+        alert("Error fetching exchange rate.");
+        console.error(error);
+    }
+}
+});        
