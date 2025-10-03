13db4212976eb43596c905b6d7cb785cbfa440d0
Min Derrida <min.derrida@gmail.com>
2025-10-03 16:10:58 +0100

    Update converter_script.js

--- prac0925_converter/converter_script.js
+++ prac0925_converter/converter_script.js
@@ -1,42 +1,39 @@
 const form = document.getElementById("currency_converter");
-form.addEventListener("submit", async function calculate() {
-
-    calculate.preventDefault();
-
-    const from_currency = document.getElementById("from_currency").value;
-    const to_currency = document.getElementById("to_currency").value;
-    const from_amount = parseFloat(document.getElementById("from_amount"));
-    const unit_symbol = document.getElementById("from_unit").value;
-    
-    if (typeof(from_amount) !== 'number' || isNaN(from_amount)) {
-        alert("Please enter an amount.");
-        return;
-    }
-
+from_currency.addEventListener("change", function() {
     const symbols = {
     USD: "$",
     EUR: "€",
     GBP: "£",
     CNY: "¥"
     };
+    document.getElementById("from_unit").textContent = symbols[from_currency.value] || "";})
+
+form.addEventListener("submit", async function calculate(event) {
+
+    event.preventDefault();
+
+    const from_currency = document.getElementById("from_currency").value;
+    const to_currency = document.getElementById("to_currency").value;
+    const from_amount = parseFloat(document.getElementById("from_amount").value);
+    
+    if (typeof(from_amount) !== 'number' || isNaN(from_amount)) {
+        alert("Please enter an amount.");
+        return;
+    }
+
 
     try {
-        const url = `https://api.frankfurter.dev/v1/latest?base=${encodeURIComponent(from)}&symbols=${encodeURIComponent(to)}`;
+        const url = `https://api.frankfurter.dev/v1/latest?base=${encodeURIComponent(from_currency)}&symbols=${encodeURIComponent(to_currency)}`;
         
-        fetch(url)
-            .then(function response(){
-                 const rate = response.rates[to_currency];
-                 const to_amount = rate * from_amount;
-                document.getElementById("result").textContent = to_amount + " " + to;)
-            }
+        const response = await fetch(url);
+        const data = await response.json();
+        const rate = data.rates[to_currency];
+        const to_amount = rate * from_amount;
+         document.getElementById("result").textContent = `${to_currency} ${to_amount.toFixed(2)}`;
              
-               
-            .catch((error) => {
+
+            } catch (error) {
                 alert("Error fetching exchange rate.");
                 console.error(error);
-            });
-    } catch (error) {
-        alert("An unexpected error occurred.");
-        console.error(error);
-    }
-});
+            }}
+);
